aa47f3e8d84c42cc872427c3dde5be2bb6e6384e
vdesai3 <vdesai3@slb.com>
2020-02-20 14:56:29 +0530

    add war file change

--- src/main/java/com/example/mongodbdemo/MongodbdemoApplication.java
+++ src/main/java/com/example/mongodbdemo/MongodbdemoApplication.java
@@ -3,14 +3,20 @@ package com.example.mongodbdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MongodbdemoApplication {
+public class MongodbdemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbdemoApplication.class, args);
 	}
 	
-	
+	 @Override
+ protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  return application.sources(DeployableWarApplication.class);
+ }
+
 
 }
